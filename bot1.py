c2ed7d41de43056595fa3fb93e4286f24a277118
Sergey Zaharco <zaharco17@gmal.com>
2021-05-20 01:11:08 +0300

    удалил ссылки на proxi

--- bot1.py
+++ bot1.py
@@ -1,33 +1,31 @@
 import logging
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, settings
-PROXY = PROXY = {'proxy_url': settings.PROXY_URL,
-    'urllib3_proxy_kwargs': {
-        'username': settings.PROXY_USERNAME,
-        'password': settings.PROXY_PASSWORD
-    }
-}
 
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+import settings
 
 logging.basicConfig(filename='bot.log', level=logging.INFO)
+
+
+def greet_user(update, context):
+    print('Вызван \start')
+    update.message.reply_text('Привет, пользователь! Ты вызвал команду /start')
 
 def talk_to_me(update, context):
     user_text = update.message.text 
     print(user_text)
     update.message.reply_text(user_text)
 
-def greet_user(update, context):
-    print('Вызван \start')
-    update.message.reply_text('Привет, пользователь! Ты вызвал команду /start')
-
-
 def main():
-    mybot = Updater(settings.API_KEY, use_context=True, request_kwargs=PROXY)
+    mybot = Updater(settings.API_KEY, use_context=True)
     
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+
     logging.info("Бот стартовал")
     mybot.start_polling()
     mybot.idle()
 
-main()
+if __name__ == "__main__":
+    main()
